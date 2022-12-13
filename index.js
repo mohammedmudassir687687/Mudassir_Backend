241e9c983584e4e917c6939cc14e67c59a31c77e
Mohammed Abbas <abbas.cloud21@gmail.com>
2022-12-13 11:52:44 -0500

    changed to node only

--- index.js
+++ index.js
@@ -1,51 +1,90 @@
-const express = require('express')
+const http = require('http')
 const fs= require("fs")
-const cors = require('cors')
 const path = require('path')
 
-const app = express()
+// const dirPath = path.join(__dirname,'/public')
 
-app.use(express.json())
+const PORT = process.env.PORT || 7000;
 
-const PORT = 7000;
+const server = http.createServer(async (req, res) => {
 
-var corsOrigin = {
-    origin:'http://localhost:5000'
-}
+    if (req.url=='/api/students')
+    {
+        fs.readFile(
+            path.join(__dirname, 'students.json'),'utf-8',
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    // Please note the content-type here is application/json
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(content);
+                        }
+              );
+    }
+    else if (req.url=='/api/faculty')
+    {
+        fs.readFile(
+            path.join(__dirname, 'grades.json'),'utf-8',
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    // Please note the content-type here is application/json
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(content);
+                        }
+              );
+    }
+    else if (req.url=='/api')
+    {
+        fs.readFile(
+            path.join(__dirname, 'students.json'),'utf-8',
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    // Please note the content-type here is application/json
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(content);
+                        }
+              );
+    }
+    else if(req.url === "/"){
+        fs.readFile("./public/index.html", "UTF-8", function(err, html){
+            // res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }
+    else if (req.url == "/style.css") {
+        fs.readFile(path.join(__dirname, "public", "style.css"), (err, content) => {
+          if (err) throw err;
+          res.writeHead(200, { "Content-Type": "text/css" });
+          res.end(content);
+        })
+    }
+    const images = [
+        "aws.png",
+        "ethereum.jpeg",
+        "pythonnode.jpeg",
+        "studying.jpg"
+    ];
+    for (const img of images) {
+        if (req.url === `/images/${img}`) {
+            fs.readFile(
+            path.join(__dirname, "public/images", img),
+            (err, content) => {
+                if (err) console.log(err);;
 
-app.use(cors(corsOrigin))
-
-app.use(cors())
-
-const dirPath = path.join(__dirname,'/public')
-
-
-app.use(express.static(dirPath))
-
-app.get("/",(req,res)=>{
-    res.sendFile(dirPath+'/index.html')
-})
+                res.writeHead(200, { "Content-Type": `image/${img.split(".")[1]}` });
+                res.end(content);
+            });
+        }
+    }
+    // // else{
+    //     res.end("<h1> 404 nothing is here</h1>");
+    // }
+    
+});
 
 
-app.get("/api",(req,res)=>{
-    var result = fs.readFileSync('./students.json')
-    parsedData = JSON.parse(result)
-    res.send(parsedData)
-})
-
-app.get("/api/students",(req,res)=>{
-    var result = fs.readFileSync('./students.json')
-    parsedData = JSON.parse(result)
-    res.send(parsedData)
-})
-
-app.get("/api/faculty",(req,res)=>{
-    var result = fs.readFileSync('./grades.json')
-    parsedData = JSON.parse(result)
-    res.send(parsedData)
-})
-
-app.listen(process.env.PORT || PORT,()=>{
-    console.log('Server started')
-})
-
+server.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+});
